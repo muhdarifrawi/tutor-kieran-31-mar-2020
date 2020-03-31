4b15d8c390e8f18edacdabace3abcb0c486acd40
muhdarifrawi <muhdarifrawi@gmail.com>
2020-03-31 16:23:37 +0800

    second phase.

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+let x = document.getElementsByTagName("div");
+
+x[0].style.color = "red";
+
+console.log(x);
+
+let y = "7";
+
+// Variables are like boxes. They hold stuff for you.
+/* this is also a comment 
+    multi-line comments are possible.
+*/
+
+// this is a variable with a string
+let z = "3";
+
+// console.log("Z:")
+// console.log(z + y);
